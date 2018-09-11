3cf1934d9553daa04c005ccbe76e5605f74384cb
aeffiong <aeffiong9@gmail.com>
2018-09-10 22:01:13 -0600

    trying hosting again

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavBar.css';
+
+// footer
+const Footer = props => (
+    <footer className="page-footer">
+        <p>Clicky Game! Thanks for playing!</p>
+
+    </footer>
+);
+
+export default Footer;
---
+++ src/components/Jumbotron.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Jumbotron.css";
+
+// jumbotron
+const Jumbotron = props => (
+    <div className="jumbotron jumbotron-fluid">
+        <div className="wrapper">
+            <h1 className="display-4">Foodie Clicky Game!</h1>
+            <p className="lead">Click on a foodie image to earn points, but don't click on any more than once!</p>
+
+        </div>
+
+    </div>
+
+);
+
+export default Jumbotron;
